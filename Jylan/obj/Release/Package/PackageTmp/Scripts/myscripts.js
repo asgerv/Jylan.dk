4f5c85bb57a50a693e89422e7e3701687c0f9cab
Asger Vestbjerg <asgerv@gmail.com>
2015-08-26 15:19:09 +0200

    hey

---
+++ Jylan/obj/Release/Package/PackageTmp/Scripts/myscripts.js
@@ -0,0 +1,21 @@
+﻿// Customized scripts
+// EDITED BY: ASGERV
+
+$(function () {
+    $(".datepick").datetimepicker({
+        showWeek: true,
+        firstDay: 1,
+        monthNames: ["Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"],
+        dateFormat: "dd-mm-yy",
+        changeYear: true
+    });
+});
+
+$(document).ready(function () {
+    var time = $("#timeleft").val();
+    $("#counter").countdown({
+        startTime: time,
+        format: 'hh:mm:ss',
+        image: '/Content/Images/digits.png'
+    });
+});
