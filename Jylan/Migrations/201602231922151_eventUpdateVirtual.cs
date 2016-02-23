1a3047bd9c541e91ff7373aed0b63781e354cb2a
Asger Vestbjerg <asgerv@gmail.com>
2016-02-23 21:31:56 +0100

    #17 created admin "panel" changes overall. all working

---
+++ Jylan/Migrations/201602231922151_eventUpdateVirtual.cs
@@ -0,0 +1,16 @@
+namespace Jylan.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class eventUpdateVirtual : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
