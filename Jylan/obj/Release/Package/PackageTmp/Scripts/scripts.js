c5e9126307bd69429282115a43bd57a541615358
Asger Vestbjerg <asgerv@gmail.com>
2016-02-25 17:20:20 +0100

    Icon fix

---
+++ Jylan/obj/Release/Package/PackageTmp/Scripts/scripts.js
@@ -0,0 +1,49 @@
+﻿$("#clock").countdown($("#eventstartdate").val(), function(event) {
+    var $this = $(this).html(event.strftime(""
+        + "<span>%D</span> dage, "
+        + "<span>%H</span> timer, "
+        + "<span>%M</span> minutter, og "
+        + "<span>%S</span> sekunder"));
+});
+
+$(function() {
+    $(".datepick").datetimepicker({
+        showWeek: true,
+        firstDay: 1,
+        monthNames: ["Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"],
+        monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"],
+        dateFormat: "dd-mm-yy",
+        changeYear: true,
+        changeMonth: true
+    });
+});
+
+$(document).ready(function() {
+    var time = $("#timeleft").val();
+    $("#counter").countdown({
+        startTime: time,
+        format: "hh:mm:ss",
+        image: "/Content/Images/digits.png"
+    });
+});
+
+$(document).ready(function() {
+    initialize();
+});
+
+function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(55.409856, 8.973934),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+        mapOptions);
+    // create a marker
+    var latlng = new google.maps.LatLng(55.409856, 8.973934);
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: "JYLAN @ Lintrup Aktivitetscenter"
+    });
+}
